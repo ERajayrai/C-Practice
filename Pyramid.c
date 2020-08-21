5afd31dc9f5840e59bebe507c0d0de5d23cb7e37
Ajay Rai <ajay.rai.1993@gmail.com>
2020-08-21 17:57:06 +0530

    first commit

---
+++ Pyramid.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n,sp;
+clrscr();
+printf("enter any number:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+    for(sp=1;sp<=n-j;sp++)
+    {
+    printf(" ");
+    }
+    for(j=1;j<=i;j++)
+    {
+    printf(" *");
+    }
+  printf("\n");
+}
+getch();
+}
