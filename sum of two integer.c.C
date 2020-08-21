b3613d27f18b336a2f112e475ae3a6133151e225
Ajay Rai <ajay.rai.1993@gmail.com>
2020-08-21 21:07:03 +0530

    sum of two integer.c file

---
+++ sum of two integer.c.C
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum;
+printf("\nenter the two intiger value:");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum is the:%d",sum);
+getch();
+}
