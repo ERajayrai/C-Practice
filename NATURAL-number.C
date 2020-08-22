2e6efdc10efe4dc45a4a5881668307159f94d5af
Ajay Rai <ajay.rai.1993@gmail.com>
2020-08-22 18:19:49 +0530

    sum of natural number

---
+++ NATURAL-number.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0;
+printf("enter a number:");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+sum=sum
++i;
+}
+printf("sum is=%d",sum);
+getch();
+}
